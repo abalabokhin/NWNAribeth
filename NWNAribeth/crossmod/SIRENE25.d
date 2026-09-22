d9e2da7fd1a363b89f48747204511fe4c344660d
CAughenbaugh <coryaughenbaugh@gmail.com>
2026-09-22 13:55:37 -0500

    Update SIRENE25.d

--- NWNAribeth/crossmod/SIRENE25.d
+++ NWNAribeth/crossmod/SIRENE25.d
@@ -15,18 +15,18 @@ CombatCounter(0)
 Global("NWSireneAribeth","GLOBAL",0)~THEN BC0SIRE2 Sirene1
 ~Sister Aribeth?~
 DO~SetGlobal("NWSireneAribeth","GLOBAL",1)~
-== BNWARIBE~Sister? I'm not a follower of the Crying God, why do you address me in that way.~
+== BNWARI25~Sister? I'm not a follower of the Crying God, why do you address me in that way.~
 == BC0SIRE2~I meant it, not as a title, but a form of address.~
-== BNWARIBE~Ok, I'm now very confused?~
+== BNWARI25~Ok, I'm now very confused?~
 == BC0SIRE2~I didn't explain myself very well. What I mean to say is that... I regard you as a sister, not just a friend.~
-== BNWARIBE~Thank you?..... But that doesn't clear up my prior confusion.~
+== BNWARI25~Thank you?..... But that doesn't clear up my prior confusion.~
 == BC0SIRE2~From the time that you joined us, I have attempted to be there for you as a friend, confidant and guide.~
 == BC0SIRE2~I have since realized that you do not need my help as a shepard.~
 == BC0SIRE2~Despite the fact that on some matters, we do not see eye to eye, I have learned more from you than you have from me.~
-== BNWARIBE~I hadn't realized that you felt this way.~
+== BNWARI25~I hadn't realized that you felt this way.~
 == BC0SIRE2~It's quite all right. Because of this, I regard you more as a sister than simply a friend.~
 == BC0SIRE2~As I have offered my support to you. I wanted to ask, may I lean on you if things become difficult?~
-== BNWARIBE~Of course you may.~
+== BNWARI25~Of course you may.~
 == BC0SIRE2~I thank you, my sister.~
-== BNWARIBE~You are welcome.... Sister.~
+== BNWARI25~You are welcome.... Sister.~
 EXIT
